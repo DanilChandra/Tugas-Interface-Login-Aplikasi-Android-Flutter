e22c76140fe070e9f8ed15509c4a00bc42b5bbc4
DanilChandra <danilchandra1809@gmail.com>
2021-02-14 01:00:11 +0700

    commit

---
+++ lib/main.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flut',
+      home: Scaffold(
+        resizeToAvoidBottomPadding: false,
+        appBar: AppBar(
+          title: Text("Login Page"),
+        ),
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomCenter,
+              stops: [0.3, 0.6, 0.9],
+              colors: [
+                Color.fromRGBO(12, 235, 235, 1),
+                Color.fromRGBO(32, 227, 178, 1),
+                Color.fromRGBO(41, 255, 198, 1),
+              ],
+            ),
+          ),
+          child: Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(12)),
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomCenter,
+                  stops: [0.3, 0.6, 0.9],
+                  colors: [
+                    Color.fromRGBO(12, 235, 235, 1),
+                    Color.fromRGBO(32, 227, 178, 1),
+                    Color.fromRGBO(41, 225, 198, 1),
+                  ],
+                ),
+                boxShadow: [
+                  BoxShadow(
+                      blurRadius: 18.0,
+                      color: Colors.transparent.withOpacity(.5),
+                      spreadRadius: 12.5),
+                ]),
+            margin: EdgeInsets.fromLTRB(40, 35, 40, 50),
+            child: Column(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(bottom: 45, top: 15),
+                  child: Column(
+                    children: <Widget>[
+                      Image.asset('assets/profile.png', width: 99, height: 45),
+                      Text(
+                        "Login",
+                        style: TextStyle(
+                            fontFamily: 'LexendDeca',
+                            fontSize: 19,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(10, 5, 10, 20),
+                  child: Column(
+                    children: <Widget>[
+                      Text("Email"),
+                      Container(
+                        margin: EdgeInsets.only(top: 10, bottom: 8),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            hintText: "Email",
+                            prefixIcon: Icon(Icons.message),
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(28)),
+                          ),
+                        ),
+                      ),
+                      Text("Password"),
+                      Container(
+                        margin: EdgeInsets.only(top: 10, bottom: 8),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            hintText: "Password",
+                            prefixIcon: Icon(Icons.lock),
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(28)),
+                          ),
+                        ),
+                      ),
+                      FlatButton(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0)),
+                        color: Colors.blue,
+                        textColor: Colors.white,
+                        child: Text('Sign In'),
+                        onPressed: () {},
+                      ),
+                      FlatButton(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0)),
+                        color: Colors.black12,
+                        textColor: Colors.white,
+                        child: Text('Forgot Password'),
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
